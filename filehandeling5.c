b90b10f0c778e4c74f715d9f26d57c55a07b0a32
maleksahil <maleksahil9315@gmail.com>
2024-06-14 09:29:10 +0530

    file_handeling

--- filehandeling5.c
+++ filehandeling5.c
@@ -1,35 +1,52 @@
 #include<stdio.h>
 #include<string.h>
+
 int main(){
 
-    int name[100];
-    int age;
-    int condition;
+     FILE  *fp;
     char s[1000];
+    char name[100];
+    char character;
+    int age;
 
-    printf("Enter your name here: ");
+    printf("enter your name: ");
     scanf("%s",&name);
 
-    printf("Enter your age here: ");
+    printf("enter your age: ");
     scanf("%d",&age);
-
-
-
-    FILE *fp;
 
     fp = fopen("example5.txt","r");
 
     if(fp == NULL){
-        printf("error");
-      }else{
-
-        if(age>=18){
-            while(fgets(s,sizeof(s),fp) != NULL){
+        printf("Error");
+    }else{
+        if(age>18){
+             while(fgets(s,sizeof(s),fp) != NULL){
                 printf("%s",s);
             }
-        }else{
-            printf("you are not eligable to use this website...");
+            printf("\n");
+
+            printf("\n press y if you accept our terms and condition and press n if you don't accept our terms and condition.: ");
+            scanf(" %c",& character);
+
+            if( character == 'y'){
+                printf("\nwelcome to our website...");
+            }else if( character == 'n'){
+                printf("\nplease accept our terms and condition to continue...");
+            }
         }
     }
    
-}
+
+    
+
+    // if(fp == NULL){
+
+    //     printf("error");
+
+    //   }else{ 
+    //         while(fgets(s,sizeof(s),fp) != NULL){
+    //             printf("%s",s);
+    //         }
+    //       } 
+       }
